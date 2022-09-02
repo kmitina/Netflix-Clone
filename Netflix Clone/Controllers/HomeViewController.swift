86aadbd79dcaa8c6c7af00c9b2faefaab5a420c2
Kris Mitina <kristmitina@gmail.com>
2022-09-02 14:25:10 +0200

    Sending URL Requests and Parsing JSON response

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -30,6 +30,7 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         
         homeFeedTable.tableHeaderView = headerView
+        getTrendingMovies()
 
     }
     
@@ -46,6 +47,17 @@ class HomeViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
+    }
+    
+    private func getTrendingMovies() {
+        APICaller.shared.getTrendingMovies { results in
+            switch results {
+            case .success(let movies):
+                print(movies)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,34 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by DVKSH on 2.09.22.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case failedToget
+}
+
+class APICaller {
+    
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
