b4c40d892edd43b29ce105cbe4c6329bc993322b
Kris Mitina <kristmitina@gmail.com>
2022-10-06 15:15:55 +0200

    Creating custom TableViewCell, Creating TitleViewModel for UpcomingTab

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by DVKSH on 6.10.22.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
